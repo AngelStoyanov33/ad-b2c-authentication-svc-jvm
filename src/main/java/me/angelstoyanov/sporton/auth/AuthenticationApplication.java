6adbdc52ba8e9da76bef8d0d05bc3bd6fcc0df85
Angel Stoyanov <aststoyanov@gmail.com>
2021-12-26 15:04:13 +0200

    [Update] Rename the default named AdB2CAuthenticationSvcApplication to AuthenticationApplication and disable self-signed SSL temporary

--- src/main/java/me/angelstoyanov/sporton/auth/AuthenticationApplication.java
+++ src/main/java/me/angelstoyanov/sporton/auth/AuthenticationApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AdB2CAuthenticationSvcApplication {
+public class AuthenticationApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AdB2CAuthenticationSvcApplication.class, args);
+		SpringApplication.run(AuthenticationApplication.class, args);
 	}
 
 }
