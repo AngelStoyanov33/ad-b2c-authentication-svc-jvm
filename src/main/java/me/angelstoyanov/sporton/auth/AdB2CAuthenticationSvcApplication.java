f261bd668207ef69d8fa81036af16ad5654a1dc5
Angel Stoyanov <aststoyanov@gmail.com>
2021-12-26 13:32:26 +0200

    [Initial] Initial commit, add plain Spring Initializr template maven structure and java .gitignore

---
+++ src/main/java/me/angelstoyanov/sporton/auth/AdB2CAuthenticationSvcApplication.java
@@ -0,0 +1,13 @@
+package me.angelstoyanov.sporton.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdB2CAuthenticationSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdB2CAuthenticationSvcApplication.class, args);
+	}
+
+}
