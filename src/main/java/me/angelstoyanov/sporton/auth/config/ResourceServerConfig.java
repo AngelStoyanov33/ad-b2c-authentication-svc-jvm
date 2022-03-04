e963c3691e8b7e28c8664b4c31f59324b4c27f82
Angel Stoyanov <aststoyanov@gmail.com>
2022-03-04 12:14:49 +0200

    [Update] Registered ResourceServerTokenServices bean in ResourceServerConfig

--- src/main/java/me/angelstoyanov/sporton/auth/config/ResourceServerConfig.java
+++ src/main/java/me/angelstoyanov/sporton/auth/config/ResourceServerConfig.java
@@ -4,11 +4,14 @@ import me.angelstoyanov.sporton.auth.validation.ClaimsValidator;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.DefaultAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtClaimsSetVerifier;
@@ -74,6 +77,15 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     @Bean
     public TokenStore tokenStore() {
         return new JwkTokenStore(keysetUri, accessTokenConverter(), claimSetVerifier());
+    }
+
+    @Bean
+    @Primary
+    public ResourceServerTokenServices tokenServices() {
+        DefaultTokenServices tokenServices = new DefaultTokenServices();
+        tokenServices.setSupportRefreshToken(true);
+        tokenServices.setTokenStore(this.tokenStore());
+        return tokenServices;
     }
 
 }
